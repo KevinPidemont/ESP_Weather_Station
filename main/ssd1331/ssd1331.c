32ad4da52ef310a9e2867eebb77e37f893b9eb4b
kevinpidemont <kevin.pidemont@gmail.com>
2024-11-24 20:07:19 +0100

    Refactor some code

--- main/ssd1331/ssd1331.c
+++ main/ssd1331/ssd1331.c
@@ -3,42 +3,34 @@
 #include <stdio.h>
 #include <driver/spi_master.h>
 
-#define CMD_DISPLAY_OFF 0xAE
-#define CMD_DISPLAY_ON 0xAF
-
-#define CMD_SET_COLUMN 0x15
-#define CMD_SET_ROW 0x75
-
-
-#define SSD1331_CMD_DRAWLINE 0x21      //!< Draw line
-#define SSD1331_CMD_DRAWRECT 0x22      //!< Draw rectangle
-#define SSD1331_CMD_FILL 0x26          //!< Fill enable/disable
-#define SSD1331_CMD_SETCOLUMN 0x15     //!< Set column address
-#define SSD1331_CMD_SETROW 0x75        //!< Set row adress
-#define SSD1331_CMD_CONTRASTA 0x81     //!< Set contrast for color A
-#define SSD1331_CMD_CONTRASTB 0x82     //!< Set contrast for color B
-#define SSD1331_CMD_CONTRASTC 0x83     //!< Set contrast for color C
-#define SSD1331_CMD_MASTERCURRENT 0x87 //!< Master current control
-#define SSD1331_CMD_SETREMAP 0xA0      //!< Set re-map & data format
-#define SSD1331_CMD_STARTLINE 0xA1     //!< Set display start line
-#define SSD1331_CMD_DISPLAYOFFSET 0xA2 //!< Set display offset
-#define SSD1331_CMD_NORMALDISPLAY 0xA4 //!< Set display to normal mode
-#define SSD1331_CMD_DISPLAYALLON 0xA5  //!< Set entire display ON
-#define SSD1331_CMD_DISPLAYALLOFF 0xA6 //!< Set entire display OFF
-#define SSD1331_CMD_INVERTDISPLAY 0xA7 //!< Invert display
-#define SSD1331_CMD_SETMULTIPLEX 0xA8  //!< Set multiplex ratio
-#define SSD1331_CMD_SETMASTER 0xAD     //!< Set master configuration
-#define SSD1331_CMD_DISPLAYOFF 0xAE    //!< Display OFF (sleep mode)
-#define SSD1331_CMD_DISPLAYON 0xAF     //!< Normal Brightness Display ON
-#define SSD1331_CMD_POWERMODE 0xB0     //!< Power save mode
-#define SSD1331_CMD_PRECHARGE 0xB1     //!< Phase 1 and 2 period adjustment
-#define SSD1331_CMD_CLOCKDIV                                                   \
-  0xB3 //!< Set display clock divide ratio/oscillator frequency
-#define SSD1331_CMD_PRECHARGEA 0x8A //!< Set second pre-charge speed for color A
-#define SSD1331_CMD_PRECHARGEB 0x8B //!< Set second pre-charge speed for color B
-#define SSD1331_CMD_PRECHARGEC 0x8C //!< Set second pre-charge speed for color C
-#define SSD1331_CMD_PRECHARGELEVEL 0xBB //!< Set pre-charge voltage
-#define SSD1331_CMD_VCOMH 0xBE          //!< Set Vcomh voltge
+#define SSD1331_CMD_DRAWLINE        0x21 //!< Draw line
+#define SSD1331_CMD_DRAWRECT        0x22 //!< Draw rectangle
+#define SSD1331_CMD_FILL            0x26 //!< Fill enable/disable
+#define SSD1331_CMD_SETCOLUMN       0x15 //!< Set column address
+#define SSD1331_CMD_SETROW          0x75 //!< Set row adress
+#define SSD1331_CMD_CONTRASTA       0x81 //!< Set contrast for color A
+#define SSD1331_CMD_CONTRASTB       0x82 //!< Set contrast for color B
+#define SSD1331_CMD_CONTRASTC       0x83 //!< Set contrast for color C
+#define SSD1331_CMD_MASTERCURRENT   0x87 //!< Master current control
+#define SSD1331_CMD_SETREMAP        0xA0 //!< Set re-map & data format
+#define SSD1331_CMD_STARTLINE       0xA1 //!< Set display start line
+#define SSD1331_CMD_DISPLAYOFFSET   0xA2 //!< Set display offset
+#define SSD1331_CMD_NORMALDISPLAY   0xA4 //!< Set display to normal mode
+#define SSD1331_CMD_DISPLAYALLON    0xA5 //!< Set entire display ON
+#define SSD1331_CMD_DISPLAYALLOFF   0xA6 //!< Set entire display OFF
+#define SSD1331_CMD_INVERTDISPLAY   0xA7 //!< Invert display
+#define SSD1331_CMD_SETMULTIPLEX    0xA8 //!< Set multiplex ratio
+#define SSD1331_CMD_SETMASTER       0xAD //!< Set master configuration
+#define SSD1331_CMD_DISPLAYOFF      0xAE //!< Display OFF (sleep mode)
+#define SSD1331_CMD_DISPLAYON       0xAF //!< Normal Brightness Display ON
+#define SSD1331_CMD_POWERMODE       0xB0 //!< Power save mode
+#define SSD1331_CMD_PRECHARGE       0xB1 //!< Phase 1 and 2 period adjustment
+#define SSD1331_CMD_CLOCKDIV        0xB3 //!< Set display clock divide ratio/oscillator frequency
+#define SSD1331_CMD_PRECHARGEA      0x8A //!< Set second pre-charge speed for color A
+#define SSD1331_CMD_PRECHARGEB      0x8B //!< Set second pre-charge speed for color B
+#define SSD1331_CMD_PRECHARGEC      0x8C //!< Set second pre-charge speed for color C
+#define SSD1331_CMD_PRECHARGELEVEL  0xBB //!< Set pre-charge voltage
+#define SSD1331_CMD_VCOMH           0xBE //!< Set Vcomh voltge
 
 /**
  * Internal variable that holds the SPI instance.
@@ -77,11 +69,11 @@ esp_err_t send_data(uint8_t data) {
 }
 
 void draw_pixel(uint8_t x, uint8_t y, uint16_t color) {
-    send_command(CMD_SET_COLUMN);
+    send_command(SSD1331_CMD_SETCOLUMN);
     send_command(x);
     send_command(x);
 
-    send_command(CMD_SET_ROW);
+    send_command(SSD1331_CMD_SETROW);
     send_command(y);
     send_command(y);
 
@@ -163,11 +155,11 @@ esp_err_t ssd1331_begin(ssd1331_desc* desc) {
 
     global_desc = *desc;
 
-        // Configurer les broches RST et DC comme sorties
+    // Configure RS (Reset) and D/C (Data/Command) pin as output
     gpio_set_direction(desc->rs, GPIO_MODE_OUTPUT);
     gpio_set_direction(desc->dc, GPIO_MODE_OUTPUT);
     
-    // Rétablir le reset
+    // Start by hardware resetting the SSD1331
     gpio_set_level(desc->rs, 0);
     vTaskDelay(100 / portTICK_PERIOD_MS);
     gpio_set_level(desc->rs, 1);
@@ -213,11 +205,11 @@ esp_err_t ssd1331_begin(ssd1331_desc* desc) {
 }
 
 void ssd1331_turn_on(void) {
-    send_command(CMD_DISPLAY_ON);
+    send_command(SSD1331_CMD_DISPLAYON);
 }
 
 void ssd1331_turn_off(void) {
-    send_command(CMD_DISPLAY_OFF);
+    send_command(SSD1331_CMD_DISPLAYOFF);
 }
 
 void ssd1331_clear_window(void) {
